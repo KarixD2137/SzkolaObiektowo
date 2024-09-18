9da0872248e49f17d43bc6a366ba58ac97da70c0
KarixD2137 <karol.kopczynski7@gmail.com>
2024-09-18 14:57:07 +0200

    Hermetyzacja

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Osoba osobaJas = new Osoba("Jaś","Kowalski",12);
         System.out.println(osobaJas.przedstawSie());
-        osobaJas.imie = "Jacek";
-        System.out.println(osobaJas.przedstawSie());
+        osobaJas.setImie("Jacek");
+        System.out.println(osobaJas.getImie());
     }
 }
--- src/Osoba.java
+++ src/Osoba.java
@@ -1,8 +1,14 @@
-public class Osoba {
-    public String imie;
-    public String nazwisko;
-    public int wiek;
+import java.util.Scanner;
 
+public class Osoba {
+
+    // Hermetyzacja
+    // modyfikator dostępu public, private, protected
+    private String imie;
+    private String nazwisko;
+    private int wiek;
+
+    // Konstruktor
     public Osoba(String imie, String nazwisko, int wiek) {
         this.imie = imie;
         this.nazwisko = nazwisko;
@@ -13,5 +19,42 @@ public class Osoba {
     public String przedstawSie()
     {
         return "Nazywam się " + imie + " " + nazwisko + " i mam " + wiek + " lat.";
+    }
+
+    // Metody dotępowe
+    // gettery i settery
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        //zabezpieczenia przed niepożądaną zmianą
+        System.out.println("Podaj hasło:");
+        Scanner klawiatura = new Scanner(System.in);
+        String haslo = klawiatura.next();
+        if(haslo.equals("qwe123")) // UWAGA! haslo == "qwe123" nie będzie działać bo string to obiekt
+        {
+            this.imie = imie;
+        }
+        else
+        {
+            System.out.println("Błędne hasło");
+        }
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public int getWiek() {
+        return wiek;
+    }
+
+    public void setWiek(int wiek) {
+        this.wiek = wiek;
     }
 }
